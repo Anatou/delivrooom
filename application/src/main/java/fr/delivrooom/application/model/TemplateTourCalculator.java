92310551f9a9f26d44e2c9c96b374af9e83ede9a
aurelianfrg <aurelianfrangin@gmail.com>
2025-10-06 20:10:25 +0200

    feat(Tour): targeted Dijkstra implementation

--- application/src/main/java/fr/delivrooom/application/model/TemplateTourCalculator.java
+++ application/src/main/java/fr/delivrooom/application/model/TemplateTourCalculator.java
@@ -1,16 +1,15 @@
 package fr.delivrooom.application.model;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.PriorityQueue;
+import java.util.*;
 
 public class TemplateTourCalculator implements TourCalculator{
 
-    protected Graphe g;
+    protected Graphe graph;
+    protected CityMap cityMap;
 
-    public TemplateTourCalculator(CityGraph g) {
+    public TemplateTourCalculator(Graphe g) {
         // create a Tour calculator with a graph
-        this.g = g;
+        this.graph = g;
     }
 
     @Override
@@ -28,31 +27,57 @@ public class TemplateTourCalculator implements TourCalculator{
         return 0;
     }
 
-    protected Path findShortestPath(int i, int j) {
-        return djikstraSearch(i, j);
+    protected HashMap<Integer, Path> findShortestPaths(int startIntersectionId , HashSet<Integer> targets) {
+
+        // as of now, only one target is acceptable
+        if (targets.size() > 1) {
+            throw new RuntimeException("Only single-target search is currently implemented");
+        }
+        return TargetedDijkstraSearch(startIntersectionId, targets);
     }
 
-    protected Path djikstraSearch(int i, int j) {
-        int n = g.getNbSommets();
+    protected HashMap<Integer, Path> TargetedDijkstraSearch(int startIntersectionId, HashSet<Integer> targets) throws RuntimeException {
+        //
+        int n = graph.getNbSommets();
         HashMap<Integer, Float> distances = new HashMap<Integer, Float>();
         HashMap<Integer, Integer> predecessors = new HashMap<Integer, Integer>();
-        PriorityQueue<Integer> queue = new PriorityQueue<Integer>();
+        PriorityQueue<Integer> queue = new PriorityQueue<Integer>(Comparator.comparing(distances::get)); // might be unsafe, TODO: test
+        HashSet<Integer> settled = new HashSet<Integer>();
+        int targetsRemaining = targets.size();
 
         // init
-        distances.put(i, 0.f);
-        predecessors.put(i, null);
-        queue.add(i);
+        distances.put(startIntersectionId, 0.f);
+        predecessors.put(startIntersectionId, null);
+        queue.add(startIntersectionId);
 
-        int reachedIntersection = i;
-        while (!queue.isEmpty() && reachedIntersection != j) {
-            reachedIntersection = queue.poll();
-            for (Integer newIntersectionId : g.arcs(reachedIntersection)) {
-                //if (distances.get()
+        int selectedIntersectionId = startIntersectionId;
+        while (!queue.isEmpty() && targetsRemaining > 0) {
+            selectedIntersectionId = queue.poll();
+            if (!settled.contains(selectedIntersectionId)) {
+                settled.add(selectedIntersectionId);
+                if (targets.contains(selectedIntersectionId)) {
+                    --targetsRemaining;
+                }
+                for (Integer newIntersectionId : graph.arcs(selectedIntersectionId)) {
+                    if (!settled.contains(newIntersectionId)) {
+                        float newDistance = distances.get(selectedIntersectionId) + graph.getCout(selectedIntersectionId, newIntersectionId);
+                        if (!distances.containsKey(newIntersectionId) || newDistance < distances.get(newIntersectionId)) {
+                            predecessors.put(newIntersectionId, selectedIntersectionId);
+                            distances.put(newIntersectionId, newDistance);
+                            queue.add(newIntersectionId);
+                        }
+                    }
+                }
             }
         }
 
+        if (targetsRemaining > 0) {
+            throw new RuntimeException("Input graph is not connex, no path could be found for at least a target");
+        }
 
+        // add a function to build Path objects to each target from predecessors
         return null;
     }
+
 
 }
