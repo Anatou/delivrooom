90fb1a7cf652ea0357d2d058fee25d303a903c5f
aurelianfrg <aurelianfrangin@gmail.com>
2025-10-06 16:32:00 +0200

    feat(model): added given TSP files and Road class

--- application/src/main/java/fr/delivrooom/application/model/CityGraph.java
+++ application/src/main/java/fr/delivrooom/application/model/CityGraph.java
@@ -2,11 +2,30 @@ package fr.delivrooom.application.model;
 
 import java.util.HashMap;
 
-public class CityGraph {
 
-    protected HashMap<Long, HashMap<Long, Integer>> adjacencyMatrix;
+public class CityGraph implements Graphe {
 
-    public CityGraph(HashMap<Long, HashMap<Long, Integer>> adjacencyMatrix) {
+    protected HashMap<Integer, HashMap<Integer, Integer>> adjacencyMatrix;
+    protected int intersectionsCount;
+
+    public CityGraph(HashMap<Integer, HashMap<Integer, Integer>> adjacencyMatrix, int intersectionsCount) {
         this.adjacencyMatrix = adjacencyMatrix;
+        this.intersectionsCount = intersectionsCount;
     }
+
+    @Override
+    public int getNbSommets() {
+        return intersectionsCount;
+    }
+
+    @Override
+    public int getCout(int i, int j) {
+        return this.adjacencyMatrix.get(i).get(j);
+    }
+
+    @Override
+    public boolean estArc(int i, int j) {
+        return this.adjacencyMatrix.containsKey(i) && this.adjacencyMatrix.get(i).containsKey(j);
+    }
+
 }
