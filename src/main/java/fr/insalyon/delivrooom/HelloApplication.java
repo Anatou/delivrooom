c0e20484cb62ad174a03b73d0857b518c8aa1ea1
Clément Grennerat <clement.grennerat@clgr.io>
2025-10-06 09:45:18 +0200

    Switching out of FXML

--- src/main/java/fr/insalyon/delivrooom/HelloApplication.java
+++ src/main/java/fr/insalyon/delivrooom/HelloApplication.java
@@ -2,18 +2,39 @@ package fr.insalyon.delivrooom;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class HelloApplication extends Application {
+
+    Label label = new Label("Hello from JavaFX Application!");
+    Button button = new Button("Say 'Hello World'");
+
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+
+
+        VBox root = new VBox();
+        root.setPadding(new Insets(20));
+        root.setAlignment(javafx.geometry.Pos.CENTER);
+        root.setSpacing(20);
+
+        button.setOnAction(e -> {
+            root.getChildren().remove(label);
+            root.getChildren().addFirst(label);
+        });
+
+        root.getChildren().add(button);
+
+
+        stage.setScene(new Scene(root, 400, 300));
         stage.setTitle("Hello!");
-        stage.setScene(scene);
         stage.show();
     }
 }
