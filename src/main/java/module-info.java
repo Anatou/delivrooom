27c65cf8fb5249b8bfe2cdbf45704b44742cac54
Clement <clement.dupic@insa-lyon.fr>
2025-10-06 10:44:33 +0200

    adding XMLReader class file that reads XML files

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,7 @@ module fr.insalyon.delivrooom {
     requires javafx.fxml;
 
     requires org.controlsfx.controls;
+    requires java.xml;
 
     opens fr.insalyon.delivrooom to javafx.fxml;
     exports fr.insalyon.delivrooom;
