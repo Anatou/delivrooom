ce881ebdd7574134eac0738bb39dbf4a470a0a26
anatou <anatole@desnot.com>
2025-10-10 11:47:16 +0200

    feat(tests): Changed data structure in test

--- application/src/test/java/TemplateTourCalculatorTests.java
+++ application/src/test/java/TemplateTourCalculatorTests.java
@@ -1,6 +1,4 @@
-import fr.delivrooom.application.model.CityGraph;
-import fr.delivrooom.application.model.Path;
-import fr.delivrooom.application.model.TemplateTourCalculator;
+import fr.delivrooom.application.model.*;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -32,10 +30,53 @@ public class TemplateTourCalculatorTests {
         }
     }
 
+    private record ManualCityMapFactory(
+            HashMap<Long, Intersection> intersections,
+            HashMap<Long, HashMap<Long, Road>> roads
+    ) {
+        static CityMap UnitSquare() {
+            Intersection inter1 = new Intersection(1, 0.0, 0.0);
+            Intersection inter2 = new Intersection(2, 1.0, 0.0);
+            Intersection inter3 = new Intersection(3, 1.0, 1.0);
+            Intersection inter4 = new Intersection(4, 0.0, 1.0);
+
+            Road road1to2 = new Road(inter1, inter2, 1.f, "road1to2");
+            Road road1to4 = new Road(inter1, inter4, 1.f, "road1to4");
+            Road road2to3 = new Road(inter2, inter3, 1.f, "road2to3");
+            Road road3to4 = new Road(inter3, inter4, 1.f, "road3to4");
+            Road road4to1 = new Road(inter4, inter1, 1.f, "road4to1");
+
+            HashMap<Long, Road> roads_from_1 = new HashMap<>();
+            roads_from_1.put(2L, road1to2);
+            roads_from_1.put(4L, road1to4);
+            HashMap<Long, Road> roads_from_2 = new HashMap<>();
+            roads_from_2.put(3L, road2to3);
+            HashMap<Long, Road> roads_from_3 = new HashMap<>();
+            roads_from_3.put(4L, road3to4);
+            HashMap<Long, Road> roads_from_4 = new HashMap<>();
+            roads_from_4.put(1L, road4to1);
+
+            return new CityMap(
+                    new HashMap<>(Map.of(
+                            1L, inter1,
+                            2L, inter2,
+                            3L, inter3,
+                            4L, inter4
+                    )),
+                    new HashMap<>(Map.of(
+                            1L, roads_from_1,
+                            2L, roads_from_2,
+                            3L, roads_from_3,
+                            4L, roads_from_4
+                    ))
+            );
+        }
+    }
+
     @Test
     public void simple_test() {
-        ManualGraph graph = ManualGraph.getUnitSquare();
-        CityGraph cityGraph = new CityGraph(graph.costs, graph.adjacencyList, graph.intersectionsCount);
+        CityMap cityMap = ManualCityMapFactory.UnitSquare();
+        CityGraph cityGraph = new CityGraph(cityMap);
         HashSet<Long> targets = new HashSet<>(Set.of(2L));
 
         TemplateTourCalculator templateTourCalculator = new TemplateTourCalculator(cityGraph);
