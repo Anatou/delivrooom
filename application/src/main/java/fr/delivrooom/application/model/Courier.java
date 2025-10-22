eccd71459ce9c4be51daa3bf1570bc2e79ba2875
Clement Dupic <clement.dupic@insa-lyon.fr>
2025-10-22 08:39:54 +0200

    feat(courier branch) : new branch to implement deliveries by couriers
    Signed-off-by: Clement Dupic <clement.dupic@insa-lyon.fr>

--- application/src/main/java/fr/delivrooom/application/model/Courier.java
+++ application/src/main/java/fr/delivrooom/application/model/Courier.java
@@ -3,9 +3,16 @@ package fr.delivrooom.application.model;
 public class Courier {
     private final int id;
     private DeliveriesDemand deliveriesDemand;
+    private TourSolution tourSolution;
 
     public Courier(int id, DeliveriesDemand deliveriesDemand) {
         this.id = id;
         this.deliveriesDemand = deliveriesDemand;
+        this.tourSolution = null;
     }
+
+    public TourSolution getTourSolution() {
+        return tourSolution;
+    }
+
 }
