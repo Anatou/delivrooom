4b42989dcd975aff5b8f2c149e987acbbc6b8c57
NEO001-2004 <nour.berdouz@gmail.com>
2025-10-24 11:36:01 +0200

    Cleaned code

--- adapter/src/main/java/fr/delivrooom/adapter/in/javafxgui/panes/sidebar/DeliveryCreationSection.java
+++ adapter/src/main/java/fr/delivrooom/adapter/in/javafxgui/panes/sidebar/DeliveryCreationSection.java
@@ -33,29 +33,18 @@ public class DeliveryCreationSection extends VBox {
         setAlignment(Pos.TOP_LEFT);
         this.controller = AppController.getController();
 
-        /*// Section title
-        Label titleLabel = new Label("New Delivery");
-        titleLabel.getStyleClass().addAll("title-4");
-
-        // State display
-        Label stateLabel = new Label("State: " + controller.getState().getClass().getSimpleName());
-        stateLabel.getStyleClass().add("text-muted");
-        controller.stateProperty().addListener((obs, oldState, newState) -> {
-            stateLabel.setText("State: " + newState.getClass().getSimpleName());
-        });*/
-
-
+        // Create button and Sprinner : select takeout intersection and its duration
         Button buttonTakeout = new Button("Select TakeOut Intersection");
         buttonTakeout.setGraphic(new FontIcon(FontAwesomeSolid.LOCATION_ARROW));
         buttonTakeout.setMaxWidth(Double.MAX_VALUE);
         Spinner<Integer> durationSpinnerTakeout = new Spinner<>(1,20,5);
         durationSpinnerTakeout.setEditable(true);
         durationSpinnerTakeout.setMinWidth(175);
-        //durationSpinnerTakeout.setMaxWidth(Double.MAX_VALUE);
         durationSpinnerTakeout.setId("durationSpinnerTakeout");
         durationSpinnerTakeout.setPromptText("Duration in minutes");
         HBox selectIntersectionTakeout = new HBox(buttonTakeout, durationSpinnerTakeout);
 
+        // Create button and Sprinner : select delivery intersection and its duration
         Button buttonDelivery = new Button("Select Delivery Intersection");
         buttonDelivery.setGraphic(new FontIcon(FontAwesomeSolid.LOCATION_ARROW));
         buttonDelivery.setMaxWidth(Double.MAX_VALUE);
@@ -66,12 +55,11 @@ public class DeliveryCreationSection extends VBox {
         durationSpinnerDelivery.setPromptText("Duration in minutes");
         HBox selectIntersectionDelivery = new HBox(buttonDelivery, durationSpinnerDelivery);
 
-
+        // Create button Confirm the choice of the selected Delivery
         Button buttonConfirmAddDelivery = new Button("Add Delivery");
         buttonConfirmAddDelivery.setMaxWidth(Double.MAX_VALUE);
         buttonConfirmAddDelivery.setGraphic(new FontIcon(FontAwesomeSolid.PLUS));
         buttonConfirmAddDelivery.getStyleClass().add("accent");
-        //buttonConfirmAddDelivery.setStyle("-fx-background-color:#A8E4A0");
 
         buttonTakeout.setOnAction(e -> {
             requestedIntersectionIsTakeout = true;
@@ -83,25 +71,13 @@ public class DeliveryCreationSection extends VBox {
             buttonDelivery.getStyleClass().add("accent");
             controller.requestIntersectionSelection();
         });
-
-
-        Label addTitle = new Label("Add Delivery");
-        addTitle.setStyle("-fx-font-weight: bold; -fx-font-size: 14;");
-        this.addDeliveryBox = new VBox(8,
-                selectIntersectionTakeout,
-                selectIntersectionDelivery,
-                buttonConfirmAddDelivery
-        );
-        //addDeliveryBox.setPadding(new Insets(10));
-        addDeliveryBox.setAlignment(Pos.CENTER_LEFT);
-
         buttonConfirmAddDelivery.setOnAction(e -> {
             int tDuration;
             int dDuration;
             tDuration = durationSpinnerTakeout.getValue();
             dDuration = durationSpinnerDelivery.getValue();
             if (takeout != null && delivery != null) {
-
+                // Add the delivery by calling the controller and reset style of buttons
                 Delivery addedDelivery = new Delivery(this.takeout, this.delivery, tDuration, dDuration);
                 controller.requestAddDelivery(addedDelivery);
                 buttonTakeout.setText("Select TakeOut Intersection");
@@ -109,21 +85,24 @@ public class DeliveryCreationSection extends VBox {
                 buttonDelivery.getStyleClass().removeAll("success","accent");
                 buttonTakeout.getStyleClass().removeAll("success","accent");
             }
-            //addDeliveryBox.setVisible(false);
-            //addDeliveryBox.setManaged(false);
         });
+
+
+        this.addDeliveryBox = new VBox(8,
+                selectIntersectionTakeout,
+                selectIntersectionDelivery,
+                buttonConfirmAddDelivery
+        );
+        addDeliveryBox.setAlignment(Pos.CENTER_LEFT);
+
 
         controller.selectedIntersectionProperty().addListener((obs, oldIntersection, newIntersection) -> {
             if (newIntersection != null) {
                 if (requestedIntersectionIsTakeout) {
-                    /*addDeliveryBox.setVisible(true);
-                    addDeliveryBox.setManaged(true);*/
                     this.takeout = newIntersection;
                     buttonTakeout.getStyleClass().add("success");
                     buttonTakeout.setText("Select Another TakeOut Intersection");
                 } else {
-                    /*addDeliveryBox.setVisible(true);
-                    addDeliveryBox.setManaged(true);*/
                     this.delivery = newIntersection;
                     buttonDelivery.getStyleClass().add("success");
                     buttonDelivery.setText("Select Another Delivery Intersection");
